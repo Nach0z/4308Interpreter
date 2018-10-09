8962fbb4901b4c032df09c161d72d3fff92d4bcd
James Wall <jwall@cosairus.com>
2018-10-08 20:29:20 -0400

    Ignored bin and other files. Removed some of the extra debug statements in TokenProvider.

--- src/util/TokenProvider.java
+++ src/util/TokenProvider.java
@@ -13,7 +13,7 @@ public class TokenProvider {
 	}
 	
 	public String nextToken() {
-		System.out.println("Begin nextToken");
+		//system.out.println("Begin nextToken");
 		String tmp = null; // this is just to store the next line of the file
 		try {
 			if(currentLine.size() == 0 && (tmp = buf.readLine()) == null) { // if we have nothing to return and the file is empty, return null
@@ -23,7 +23,7 @@ public class TokenProvider {
 					currentLine = new ArrayList<String>(Arrays.asList(tmp.split(" "))); // woo. we already read the next line. Turn it into an arraylist.
 				String word = currentLine.get(0); // store the first lex
 				currentLine.remove(0); // remove it from the line buffer
-				System.out.println("Popped " + word);
+				//system.out.println("Popped " + word);
 				if(LexAnalyzer.allLexes.stream().filter(x -> x.LexToken != null && x.LexToken.equals(word)).collect(Collectors.toList()).size() > 0)
 					return word; // shortcut for full-word known lexes
 				String ret = "";
@@ -47,19 +47,19 @@ public class TokenProvider {
 							// currentLine.add(0, word.replaceFirst(ret, ""));
 							{
 								currentLine.add(0, word.substring(ret.length(), word.length()));
-								System.out.println("Word not used up, re-pushing " + currentLine.get(0));
+								//system.out.println("Word not used up, re-pushing " + currentLine.get(0));
 								break;
 							}
 					}
 				}
-				System.out.println("Returning " + ret);
+				//system.out.println("Returning " + ret);
 				return ret; // give lex pls
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 			return null;
 		} finally {
-			System.out.println("End nextToken");
+			//system.out.println("End nextToken");
 		}
 	}
 }
