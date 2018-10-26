3b9a5314f7496ace72e6b8440ba37d54bef06e0b
Greenman23 <32344310+Greenman23@users.noreply.github.com>
2018-10-26 17:24:08 -0400

    added arthmetic exo
    added class

---
+++ julia_interpreter_python/ArithmeticOperators.py
@@ -0,0 +1,13 @@
+from enum import Enum
+class ArithmeticOperators(Enum):
+    
+    def __init__(self):
+        self.ADDITION = 25
+        self.SUBTRACTION = 26
+        self.MULTIPlICATION = 27
+        self.REVERSE_DIVISION = 28
+        self.DIVISION = 29
+        self.MODULAR = 30 
+        self.EXP = 31
+
+
