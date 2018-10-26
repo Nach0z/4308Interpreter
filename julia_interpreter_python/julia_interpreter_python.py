06476a1943e1e8e2e468bc039112d34347a130a4
Kurt-F <truksaver@gmail.com>
2018-10-26 16:56:36 -0400

    fixed main interpreter file

--- julia_interpreter_python/julia_interpreter_python.py
+++ julia_interpreter_python/julia_interpreter_python.py
@@ -1,2 +1,33 @@
-class julia_interpreter_python():
-    """Main method where the program runs"""
+import sys
+import os.path
+def main():
+	name = ""
+	try:
+		name = sys.argv[1]
+	except IndexError:
+		print("Please provide a file to be executed.") 
+		sys.exit()
+	if(not os.path.isfile(name)):
+		print("Specified input file not found.")
+		sys.exit()
+	try:
+		p = Parser()
+		program = p.parse()
+		program.execute()
+	except ParseException as e:
+		# Handle a parse exception
+		pass
+	except LexicalException as e:
+		# handle lex exceptions
+		pass
+	except EmptyTokenListException as e:
+		# handle empty token list
+		pass
+	except:
+		print("Unknown error occered.")
+		sys.exit()
+
+
+
+if(__name__ == "__main__"):
+	main()
