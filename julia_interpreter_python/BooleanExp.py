9e666dc28e53d249d1301f9536306692718f0703
Greenman23 <32344310+Greenman23@users.noreply.github.com>
2018-10-26 18:12:14 -0400

    Added a BooleanExp class logic
    i added logic to the BooleanExp class but its not enitrely completly. Missing what to arithmetic object

--- julia_interpreter_python/BooleanExp.py
+++ julia_interpreter_python/BooleanExp.py
@@ -1,4 +1,37 @@
-class BooleanExp(object):
-    """description of class"""
+from julia_interpreter_python import relationop
+class BooleanExp(self):
+    
+    o = relationop
+    a
+    b
 
+    """don't know what to do about arithmetic expression object"""
+    
+    def __init__(self, operator, a, b):
+        if op == null:
+            raise ValueError("operator does not value")
+        self.o = operator
+        
+        if a == null:
+            raise ValueError("the first expression does not have a value")
+        self.a = a
+
+        if b == null:
+            raise ValueError("the secound does not have a value")
+
+    def evaluate():
+        boolean = false 
+        if(o == 36):
+            boolean = a.evaluate() == b.evaluate()
+        elif(o == 37):
+            boolean = a.evaluate() != b.evaluate()
+        elif(o == 38):
+            boolean = a.evaluate() < b.evaluate()
+        elif(o == 39):
+            boolean = a.evaluate() <= b.evaluate()
+        elif(o == 40):
+            boolean = a.evaluate() > b.evaluate()
+        elif(o == 41):
+            boolean = a.evaluate() >= b.evaluate()
+        return boolean
 
---
+++ julia_interpreter_python/relationop.py
@@ -0,0 +1,13 @@
+from enum import Enum
+class relationop(enum):
+    
+    def __init__(self):
+        self.EQ = 36
+        self.NE = 32
+        self.LT = 38
+        self.LE = 39
+        self.OP = 40
+        self.GT = 41
+        self.GE = 42
+
+
