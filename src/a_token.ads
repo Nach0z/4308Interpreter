889747b12bebe92c5d3c76b4bd2afb1aeffad445
Greenman23 <32344310+Greenman23@users.noreply.github.com>
2018-12-05 21:07:26 -0500

    need to figure out how to detemine lenght of stream

--- src/a_token.ads
+++ src/a_token.ads
@@ -1,5 +1,69 @@
+use Ada.Exceptions
+use Token;
+  
 package a_token is
-
+   
+   type a_token is new Controlled with public;
+   
+   token_ID : Integer;
+   lex : String;
+   row : Integer;
+   column : Integer;
+   
+   function Create(token_ID : Integer, lex : String, row : Integer, column : Integer) return a_token is 
+      
+      if token_ID == null then 
+         raise Invalid_Argument with "one of the token value is invalid!";
+         
+      elsif lex == null then 
+         raise Invalid_Argument with "one of the token value is invalid!";
+          
+      elsif row == null then
+         raise Invalid_Argument with "invalid row!";
+                 
+      elsif column == null then
+         raise Invalid_Argument with "invalid column";
+         
+      else
+         token_ID := token_ID;
+         lex := String;
+         row := Integer;
+         column := Integer;
+      end if;
+      
+   end Create;
+   
+                          
+   --TODO: Find a way to access the variables addressed below. Created a contructor like instance but need to 
+   --      figure out how to use those variables created.
+   function get_token_type return Integer is 
+      
+      return token_type'Val(token_ID);
+      
+   end get_token_type;
+   
+   
+   function get_row return Integer is 
+      
+      return row;
+      
+   end get_row;
+   
+   
+   function get_Col return Integer is
+      
+      return column;
+      
+   end get_Col;
+   
+   
+   function get_lex return String is 
+      
+      return lex;
+      
+   end get_lex;
+   
+   --TODO: implement a str testing method like the one from the python version
    
 
 end a_token;
